316a9537f0bd47e2a9eaf5c34bed8b74143f5ad4
wagyourtail <ruwagray@gmail.com>
2019-12-09 15:22:13 -0700

    better fly
    making fly work better

--- hacks/Fly.lua
+++ hacks/Fly.lua
@@ -16,18 +16,22 @@ local CPlayerPacket = luajava.bindClass("net.minecraft.network.play.client.CPlay
 local nextKey = false
 local descTime = 0
 local up = false
+local topShift = 0
 while FLY_ENABLE do
+    local x,y,z = getPlayerPos()
+    
+    
          --minecraft.player.abilities.canFly
     if not mc.field_71439_g.field_71075_bZ.field_75101_c then
       --minecraft.player.abilities.canFly
         mc.field_71439_g.field_71075_bZ.field_75101_c = true
     end
     
+    
+    
+ -- BYPASS FLY NOT ENABLED ON THIS SERVER --
                         --minecraft.player.abilities.isFlying
-    if descTime >= 10 and mc.field_71439_g.field_71075_bZ.field_75100_b then -- no fall
-        local x,y,z = getPlayerPos()
-        
-        -- BYPASS FLY NOT ENABLED ON THIS SERVER
+    if descTime >= 10 and mc.field_71439_g.field_71075_bZ.field_75100_b then
         if up then
         --minecraft.player.connection.sendPacket(new CPlayerPacket.PositionPacket(true))
           mc.field_71439_g.field_71174_a:func_147297_a(luajava.new(CPlayerPacket.PositionPacket, x, y+.05, z, true))
@@ -36,19 +40,21 @@ while FLY_ENABLE do
           mc.field_71439_g.field_71174_a:func_147297_a(luajava.new(CPlayerPacket.PositionPacket, x, y-.05, z, true))
         end
         up = not up
-        
-        --minecraft.player.fallDistance
-        mc.field_71439_g.field_70143_R = 0
         descTime = 0
+        mc.field_71439_g.field_70143_R = 0
     end
     
-    --minecraft.player.fallDistance and !minecraft.player.isElytraFlying()
-    if mc.field_71439_g.field_70143_R > 2 and not mc.field_71439_g:func_184613_cA() then
-      --minecraft.player.connection.sendPacket(new CPlayerPacket.PositionPacket(true))
+ -- NO FALL --
+    --(minecraft.player.fallDistance or "descending with shift key")and !minecraft.player.isElytraFlying()
+    if (mc.field_71439_g.field_70143_R > 2 or isKeyDown("LEFT_SHIFT")) and not mc.field_71439_g:func_184613_cA() then
+      --minecraft.player.connection.sendPacket(new CPlayerPacket(true))
         mc.field_71439_g.field_71174_a:func_147297_a(luajava.new(CPlayerPacket, true))
         --minecraft.player.fallDistance
         mc.field_71439_g.field_70143_R = 0
+        topShift = y
     end
+    
+    
     descTime = descTime + 1
     waitTick()
 end
